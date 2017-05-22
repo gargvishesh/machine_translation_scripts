7d23b9ce586e08e9e11f54bae009bd6ddf9bdd19
gargvishesh <gargvishesh@gmail.com>
2017-05-22 15:57:07 +0530

    Added windows support

--- do_something_on_files.py
+++ do_something_on_files.py
@@ -5,15 +5,17 @@ import getopt
 import utilities
 import io
 import textract
+import os
 
+dir_path_demarker = ""
 
 def remove_page_number(infile, indir, outdir, language):
     if not indir == None:
-        infile_path = indir + '/' + infile
+        infile_path = indir + dir_path_demarker + infile
     else:
         infile_path = infile
     fin = io.open(infile_path, "r")
-    outfile_path = outdir + '/' + infile + '.out'
+    outfile_path = outdir + dir_path_demarker + infile + '.out'
     fout = io.open(outfile_path, "w")
     for line in fin:
         pos = line.find("Babuji")
@@ -22,21 +24,24 @@ def remove_page_number(infile, indir, outdir, language):
                 fout.write(u"Babuji\n")
                 continue
             else:
-                print "Rightmost char: ", (line.rstrip())[-1]
+                print ("Rightmost char: " + (line.rstrip())[-1])
         fout.write(line)
 
 def extract_from_pdf(infile, indir, outdir, language):
+    print ("Demarker:", dir_path_demarker)
     if not indir == None:
-        infile_path = indir + '/' + infile
+        print (indir, ":",dir_path_demarker, ":",infile)
+        infile_path = indir + dir_path_demarker + infile
     else:
         infile_path = infile
     #fin = io.open(infile_path, "r")
+    print ("Infile path:", infile_path);
     outfile = infile + '.raw.txt'
 
     if not outdir == None:
-        outfile_path = outdir + '/' + outfile
+        outfile_path = outdir + dir_path_demarker + outfile
     elif not indir == None:
-        outfile_path = indir + '/' + outfile
+        outfile_path = indir + dir_path_demarker + outfile
     else:
         outfile_path = outfile
 
@@ -49,19 +54,19 @@ def extract_from_pdf(infile, indir, outdir, language):
 
 def combine_paragraph(infile, indir, outdir, language):
 
-    print 'Input file:', infile
-    print 'Language:', language
+    print ("Input file:", infile)
+    print ("Language:", language)
 
     if language != "tamil" and language != "hindi":
         sys.stderr.write("Language not supported\n")
         exit(1)
 
     if not indir == None:
-        infile_path = indir + '/' + infile
+        infile_path = indir + dir_path_demarker + infile
     else:
         infile_path = infile
     fin = io.open(infile_path, "r")
-    outfile_path = outdir + '/' + infile + '.out'
+    outfile_path = outdir + dir_path_demarker + infile + '.out'
     fout = io.open(outfile_path, "w")
     accumulated = ""
     ignore_lines_count = 0
@@ -93,9 +98,9 @@ def do_some_operation(infile, indir, outdir, language):
 
     # In batch mode, the input file consists of a newline separated list of file names containing the source text
     if indir:
-        print "**********************"
-        print "Directory Mode Enabled"
-        print "**********************"
+        print ("**********************")
+        print ("Directory Mode Enabled")
+        print ("**********************")
 
 
     filenames = []
@@ -111,10 +116,10 @@ def do_some_operation(infile, indir, outdir, language):
         extract_from_pdf(filename, indir, outdir, language)
 
 def print_usage(binary_name):
-    print 'Usage:', binary_name, "--infile=<inputfile> --indir=<input dir> --outdir=<output dir> -l=<hindi/tamil>"
-    print "--infile: Take just a given file for processing"
-    print "--indir: Take all files in the dir for processing "
-    print "--outdir: Puts all generated files in the output dir. Default is the same as the input path"
+    print ("Usage:", binary_name, "--infile=<inputfile> --indir=<input dir> --outdir=<output dir> -l=<hindi/tamil>")
+    print ("--infile: Take just a given file for processing")
+    print ("--indir: Take all files in the dir for processing ")
+    print ("--outdir: Puts all generated files in the output dir. Default is the same as the input path")
     sys.exit(2)
 
 
@@ -124,6 +129,7 @@ def main(binary_name, argv):
     outdir = None
 
     global language
+    global dir_path_demarker
 
     if len(argv) == 0:
         print_usage(binary_name)
@@ -133,7 +139,7 @@ def main(binary_name, argv):
         opts, args = getopt.getopt(argv, "hl:", ["help", "infile=", "indir=",
                                                  "outdir="])
     except getopt.GetoptError:
-        print "Cannot parse arguments. Re-check flags"
+        print ("Cannot parse arguments. Re-check flags")
         print_usage(binary_name)
 
     for opt, arg in opts:
@@ -149,16 +155,23 @@ def main(binary_name, argv):
             outdir = arg
 
     if infile is None and indir is None:
-        print "Input file / Dir not specified"
+        print ("Input file / Dir not specified")
         print_usage(binary_name)
     elif language is None:
-        print "Language not specified"
+        print ("Language not specified")
         print_usage(binary_name)
 
-    print 'Input file:', infile
-    print 'Input Dir:', indir
-    print 'Output Dir:', outdir
-    print 'Language:', language
+    if os.name == 'nt':
+        dir_path_demarker = '\\'
+    else:
+        dir_path_demarker='/'
+    print ("OS Name is:", os.name, "demarker:", dir_path_demarker)
+    
+
+    print ("Input file:", infile)
+    print ("Input Dir:", indir)
+    print ("Output Dir:", outdir)
+    print ("Language:", language)
 
     do_some_operation(infile, indir, outdir, language)
 
